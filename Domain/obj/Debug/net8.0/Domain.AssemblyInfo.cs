e3dcff9e48e71eb09e1f2435e88dae9b83614d01
isroilov1 <isroilov0905@gmail.com>
2024-06-15 11:27:03 +0500

    423t45y

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c673e23bf930b371509521a87f63616c7f18f3")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d690adf1bde3cce7494e6b0ad8781d615e9b9d")]
->>>>>>> 83077ba36287467d0854839b78bdc90e4fd06291
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0722177611c6e0d736f051a535ad5c02b7e7406c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
