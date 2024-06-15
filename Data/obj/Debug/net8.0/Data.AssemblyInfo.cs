c0c673e23bf930b371509521a87f63616c7f18f3
isroilov1 <isroilov0905@gmail.com>
2024-06-15 09:19:30 +0500

    sdfdg

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1fece674b4a494c43b0b092525fe20528b3516c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869ce3c6734a271c0c09a5304502e329049da95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PresentationCreator/obj/Debug/net8.0/PresentationCreator.AssemblyInfo.cs
+++ PresentationCreator/obj/Debug/net8.0/PresentationCreator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationCreator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1fece674b4a494c43b0b092525fe20528b3516c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869ce3c6734a271c0c09a5304502e329049da95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationCreator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationCreator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
