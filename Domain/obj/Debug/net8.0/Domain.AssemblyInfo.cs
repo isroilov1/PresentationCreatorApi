f1205e67e8ee7396d18a63f8bff8bb595575c844
isroilov1 <isroilov0905@gmail.com>
2024-06-15 11:56:34 +0500

    u

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60a44372ba0e990ab4b974f99078e2ef0c2cbd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66597a3ca5948aa8ef8182543fb63560fe37b08b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
