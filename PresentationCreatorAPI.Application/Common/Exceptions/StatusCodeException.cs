06d5b6580d67096824d637f32d7711e81902c5e9
Saidjon <saidjonnozimboyevv@gmail.com>
2024-06-20 12:16:30 +0500

    application has started

---
+++ PresentationCreatorAPI.Application/Common/Exceptions/StatusCodeException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace PresentationCreatorAPI.Application.Common.Exceptions;
+
+public class StatusCodeException : Exception
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public StatusCodeException(HttpStatusCode code, string message)
+        : base(message)
+    {
+        StatusCode = code;
+    }
+}
---
+++ PresentationCreatorAPI.Application/Common/Helpers/FileHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PresentationCreatorAPI.Application.Common.Helpers;
+
+public static class FileHelper
+{
+    public static string SaveFile(IFormFile file, string rootPath)
+    {
+        if (file == null || file.Length == 0)
+            throw new ArgumentException("File is invalid.");
+
+        // Fayl yo'li mavjud bo'lmasa, uni yaratish
+        if (!Directory.Exists(rootPath))
+            Directory.CreateDirectory(rootPath);
+
+        // Generate unique file name to avoid conflicts
+        var fileName = Path.GetFileNameWithoutExtension(file.FileName) + "_" + Guid.NewGuid() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(rootPath, fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            file.CopyTo(stream);
+        }
+
+        return filePath;
+    }
+
+    private static async Task<string> SavePaymentFileAsync(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            throw new ArgumentException("File is invalid.");
+
+        var rootPath = Path.Combine("wwwroot", "uploads", "payments");
+        // Fayl yo'li mavjud bo'lmasa, uni yaratish
+        if (!Directory.Exists(rootPath))
+            Directory.CreateDirectory(rootPath);
+
+        var filePath = Path.Combine(rootPath, file.FileName);
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return filePath;
+    }
+}
