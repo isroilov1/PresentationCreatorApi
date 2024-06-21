276e532d9c1813dc4668a9f13be9a9b471cd9539
isroilov1 <isroilov0905@gmail.com>
2024-06-21 16:18:42 +0500

    ok

---
+++ PresentationCreatorAPI.Application/Services/PresentationServise.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs.PageDtos;
+using FluentValidation;
+using PresentationCreatorAPI.Application.Common.Validators;
+using PresentationCreatorAPI.Application.DTOs.PresentationDtos;
+using PresentationCreatorAPI.Application.Interfaces;
+using PresentationCreatorAPI.Application.presntations.Presentationpresntations;
+using PresentationCreatorAPI.Data.Interfaces;
+using PresentationCreatorAPI.Entites;
+
+namespace PresentationCreatorAPI.Application.Services;
+
+public class PresentationServise(IUnitOfWork unitOfWork,
+                                 IValidator<Presentation> validator) : IPresentationServise
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IValidator<Presentation> _validator = validator;
+
+    public async Task CreateAsync(AddPresentationDto dto)
+    {
+        var result = await _validator.ValidateAsync(dto);
+        if (!result.IsValid)
+            throw new ValidationException(result.GetErrorMessages());
+
+        var presentation = (Presentation)dto;
+
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<PresentationDto>> GetAllPagesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PageDto> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(UpdatePresentationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
