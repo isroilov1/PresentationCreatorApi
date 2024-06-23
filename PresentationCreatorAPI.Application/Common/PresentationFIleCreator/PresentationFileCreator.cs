5b3abd4a7f3290a144c93a7b50d9967829ca74d8
isroilov1 <isroilov0905@gmail.com>
2024-06-23 19:55:48 +0500

    s

--- PresentationCreatorAPI.Application/Common/PresentationFIleCreator/PresentationFileCreator.cs
+++ PresentationCreatorAPI.Application/Common/PresentationFIleCreator/PresentationFileCreator.cs
@@ -5,55 +5,30 @@ namespace PresentationCreatorAPI.Application.Common.Helpers;
 
 using Aspose.Slides;
 using Aspose.Slides.Export;
-using OpenXmlPowerTools;
 
 public class PresentationFileCreator
 {
     public static void CreatePresentation(PresentationEntity presentationEntity)
     {
-        // Создание экземпляра объекта Presentation, представляющего файл презентации
-        using (Presentation presentation = new Presentation())
+        // Create presentation
+        using (var p = new Presentation())
         {
-            // Получить первый слайд
-            ISlide slide = presentation.Slides[0];
+            // Read image
+            var svgContent = File.ReadAllText("image.svg");
 
-            // Добавить содержимое на слайд...
-            // Получить первый слайд
-            ISlide sld = presentation.Slides[0];
+            // Add image to image collection
+            //var emfImage = p.Images.AddFromSvg(svgContent);
 
-            // Добавьте автофигуру прямоугольного типа
-            IAutoShape ashp = sld.Shapes.AddAutoShape(ShapeType.AlternateProcessFlow, 160, 75, 150, 50);
-
-            // Добавьте TextFrame в прямоугольник
-            ashp.AddTextFrame("Salomatlik");
-
-            // Доступ к текстовому фрейму
-            ITextFrame txtFrame = ashp.TextFrame;
-
-            // Создайте объект Paragraph для текстового фрейма
-            IParagraph para = txtFrame.Paragraphs[0];
-
-            // Создать объект части для абзаца
-            IPortion portion = para.Portions[0];
-
-            // Установить текст
-            portion.Text = "Aspose TextBox";
-
-
-
-            // Читать изображение
-            // EMF rasm faylini o'qish
             var emfContent = File.ReadAllBytes("wwwroot/assets/templates/1/1.png");
 
             // EMF rasmni Aspose.Slides presentationsiga qo'shish
-            var emfImage = presentation.Images.AddImage(new MemoryStream(emfContent));
+            var emfImage = p.Images.AddImage(new MemoryStream(emfContent));
 
-            // Добавить изображение на слайд
-            presentation.Slides[0].Shapes.AddPictureFrame(ShapeType.Rectangle, 0, 0, 16, 9, emfImage);
+            // Add image to slide
+            p.Slides[0].Shapes.AddPictureFrame(ShapeType.Rectangle, 0, 0, emfImage.Width, emfImage.Height, emfImage);
 
-
-            // Сохранить презентацию
-            presentation.Save("uploads/presentations/1/NewPresentation.pptx", SaveFormat.Pptx);
+            // Save presentation
+            p.Save("presentation.pptx", SaveFormat.Pptx);
+        }
         }
     }
-}
